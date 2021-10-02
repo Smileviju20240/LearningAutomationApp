e3748120257bb29670b47c2b67d5ab9cd9a2c345
Vijay Dhage <Vijay.Dhage@blueconchtech.com>
2021-10-02 19:41:30 +0530

    locators added

--- src/main/java/PageClasses/OurProjectPage.java
+++ src/main/java/PageClasses/OurProjectPage.java
@@ -11,6 +11,9 @@ public class OurProjectPage {
 	WebDriver driver;
 	
 	By Practice1 = By.cssSelector("a[href='https://rahulshettyacademy.com/seleniumPractise/#/']");
+	By Practise2 = By.xpath("//a[text()='Automation Practise - 2']");
+	By AndroidApp_link = By.xpath("//a[text()='Hybrid Android App']");
+	By API_contract_Tab = By.xpath("//a[text()='Library API Contract']");
 	
 	
 	// Always Create constructor of page class because it will always be called in other class while object creation.
